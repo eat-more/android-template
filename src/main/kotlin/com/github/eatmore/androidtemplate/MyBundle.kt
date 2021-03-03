c30733a94788089333fb76f65c692eef77e76aa3
GitHub Action <action@github.com>
2021-03-03 03:14:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/eatmore/androidtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/eatmore/androidtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.eatmore.androidtemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/eatmore/androidtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/eatmore/androidtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.eatmore.androidtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.eatmore.androidtemplate.MyBundle
 
 class MyApplicationService {
 
